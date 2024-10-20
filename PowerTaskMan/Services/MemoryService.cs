985330866afab617ecf0e589405f3ac7d5525482
Aaron Suydam <suydam.aaron@gmail.com>
2024-10-20 15:42:10 -0400

    Work on adding a view model

--- PowerTaskMan/Services/MemoryService.cs
+++ PowerTaskMan/Services/MemoryService.cs
@@ -13,33 +13,35 @@ using System.Threading;
 using System.Threading.Tasks;
 using LiveChartsCore;
 using System.Collections.ObjectModel;
+using System.Xml.Serialization;
+
+namespace PowerTaskMan.Performance
+{
+    public class MemoryStats
+    {
+        public int UsedMemoryMB { get; set; }
+        public int TotalMemoryMB { get; set; }
+        public int AvailableMemoryMB { get; set; }
+        public int UsedMemoryPercent { get; set; }
+    }
+
+}
+
+
+
 
 namespace PowerTaskMan.Services
 {
-    public partial class MemoryService : ObservableObject
+    public partial class MemoryService
     {
-        PerformanceCounter availableMemory = new PerformanceCounter("Memory", "Available MBytes");
+        PerformanceCounter availableMemoryPC = new PerformanceCounter("Memory", "Available MBytes");
 
-        CancellationTokenSource memoryMonitoring = new();
+        CancellationTokenSource memoryMonitoring =  new();
 
-        public ObservableCollection<ISeries> MemoryUseChartSeries { get; set; } = new();
-        Collection<int> memoryUseHistory = new();
-
-
-        [ObservableProperty]
-        int availableMemoryMB = 0;
-
-        [ObservableProperty]
-        int totalMemoryMB = 0;
-
-        [ObservableProperty]
-        int usedMemoryMB = 0;
-
-        [ObservableProperty]
-        int usedMemoryPercent = 0;
-
-        [ObservableProperty]
-        string usedMemoryMBString = "";
+        private int usedMemoryMB = 0;
+        private int totalMemoryMB = 0;
+        private int availableMemory = 0;
+        private int usedMemoryPercent = 0;
 
 
         public MemoryService()
@@ -62,65 +64,48 @@ namespace PowerTaskMan.Services
             }
         }
 
-        public void StartMemoryMonitoring(DispatcherQueue dq)
-        {
-            Task.Run(() =>
-            {
-                while (!memoryMonitoring.Token.IsCancellationRequested)
-                {
-                    dq.TryEnqueue(() =>
-                    {
-                        MemoryStatsMonitor();
-                        memoryUseHistory.Add(UsedMemoryPercent);
-                        UsedMemoryMBString = UsedMemoryMB.ToString() + " MB";
-                        UpdateChart();
-                    });
-                    Debug.WriteLine("Memory Stats: " + usedMemoryMB + "MB / " + totalMemoryMB + "MB (" + usedMemoryPercent + "%)");
-                    Thread.Sleep(500);
-                }
-            });
-
-        }
-
-        public void StopMemoryMonitoring()
-        {
-            memoryMonitoring.Cancel();
-        }
 
         private void MemoryStatsMonitor()
         {
-            AvailableMemoryMB = (int)availableMemory.NextValue();
-            UsedMemoryMB = TotalMemoryMB - AvailableMemoryMB;
-            UsedMemoryPercent = (int)(((double)UsedMemoryMB / (double)TotalMemoryMB) * 100);
+            availableMemory = (int)availableMemoryPC.NextValue();
+            usedMemoryMB = totalMemoryMB - availableMemory;
+            usedMemoryPercent = (int)(((double)usedMemoryMB / (double)totalMemoryMB) * 100);
         }
 
-
-        void UpdateChart()
+        public int GetUsedMemoryPercent()
         {
-            if (MemoryUseChartSeries.Count == 0)
-            {
-                MemoryUseChartSeries.Add(
-                    new LineSeries<int>
-                    {
-                        Values = memoryUseHistory,
-                        Stroke = new SolidColorPaint(SKColor.Parse("FFAD1EFE")) { StrokeThickness = 2 },
-                        Fill = new SolidColorPaint(SKColor.Parse("80AD1EFE")),
-                        GeometryFill = new SolidColorPaint(SKColor.Parse("FFAD1EFE")),
-                        GeometryStroke = new SolidColorPaint(SKColor.Parse("FFAD1EFE")),
-                        GeometrySize = 5,
-                        LineSmoothness = 0.1
-                    }
-                );
-            }
-            else
-            {
-                MemoryUseChartSeries[0].Values = memoryUseHistory.Select(x => x).TakeLast(60).ToArray();
-            }
-
-
-
-
+            MemoryStatsMonitor();
+            return usedMemoryPercent;
         }
 
+        public int GetUsedMemory()
+        {
+            MemoryStatsMonitor();
+            return usedMemoryMB;
+        }
+
+        public int GetTotalMemory()
+        {
+            return totalMemoryMB;
+        }
+
+        public int GetAvailableMemory()
+        {
+            MemoryStatsMonitor();
+            return availableMemory;
+        }
+
+        public Performance.MemoryStats GetAllStats()
+        {
+            MemoryStatsMonitor();
+            Performance.MemoryStats stats = new()
+            {
+                AvailableMemoryMB = availableMemory,
+                TotalMemoryMB = totalMemoryMB,
+                UsedMemoryMB = usedMemoryMB,
+                UsedMemoryPercent = usedMemoryPercent
+            };
+            return stats;
+        }
     }
 }
---
+++ PowerTaskMan/ViewModels/PerformanceViewModel.cs
@@ -0,0 +1,93 @@
+﻿using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView;
+using Microsoft.UI.Xaml;
+using power_task_man.Services;
+using PowerTaskMan.Services;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveChartsCore;
+using System.Collections.ObjectModel;
+using Microsoft.UI.Dispatching;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PowerTaskMan.ViewModels
+{
+    public partial class PerformanceViewModel : ObservableObject
+    {
+        public CPUPerfService cpuPerfService { get; set; } = new();
+        public MemoryService memoryService { get; set; } = new();
+
+        public string CPUFrequency { get; set; } = "Something";
+
+        [ObservableProperty]
+        string usedMemoryMBString = "";
+
+        [ObservableProperty]
+        int availableMemoryMB = 0;
+
+        [ObservableProperty]
+        int totalMemoryMB = 0;
+
+        [ObservableProperty]
+        int usedMemoryMB = 0;
+
+        [ObservableProperty]
+        int usedMemoryPercent = 0;
+
+
+        public ObservableCollection<ISeries> MemoryUseChartSeries { get; set; } = new();
+
+        public PerformanceViewModel()
+        {
+            
+        }
+
+        public void BeginMonitoring(DispatcherQueue dq)
+        {
+
+        }
+
+
+        void UpdateMemoryChartData()
+        {
+            if (MemoryUseChartSeries.Count == 0)
+            {
+                MemoryUseChartSeries.Add(
+                    new LineSeries<int>
+                    {
+                        Values = memoryService.memoryUseHistory.ToArray(),
+                        Stroke = new SolidColorPaint(SKColor.Parse("FFAD1EFE")) { StrokeThickness = 2 },
+                        Fill = new SolidColorPaint(SKColor.Parse("80AD1EFE")),
+                        GeometryFill = new SolidColorPaint(SKColor.Parse("FFAD1EFE")),
+                        GeometryStroke = new SolidColorPaint(SKColor.Parse("FFAD1EFE")),
+                        GeometrySize = 5,
+                        LineSmoothness = 0.1
+                    }
+                );
+                UsedMemoryMBString = UsedMemoryMB.ToString() + " MB";
+
+            }
+            else
+            {
+                MemoryUseChartSeries[0].Values = memoryUseHistory.Select(x => x).TakeLast(60).ToArray();
+                UsedMemoryMBString = UsedMemoryMB.ToString() + " MB";
+
+            }
+
+
+
+
+        }
+
+
+    }
+}
+
+
+
+    }
+}
